d9631d0864920f400243ce3d3db43c104812d1c4
tyfnlk <terryleeshanok@outlook.com>
2023-03-22 16:48:55 -0400

    Testfile for request classes

---
+++ src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+	public static void main(String[] args) {
+		TestMessage t1 = new TestMessage("Hello");
+		
+		System.out.println(t1.getMessage());
+		System.out.println(t1.getType());
+		
+	}
+
+}
